cb137bbc71f34394f8f50cf6993d78a287964362
gabrdgs <99189378+gabInteli@users.noreply.github.com>
2024-02-19 01:42:31 -0300

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,13 +1,16 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const organizationName = 'gabInteli'; // Usually your GitHub org/user name.
+const projectName = 'M9-Inteli-Eng-Comp_Gabriela_Matias'; // Usually your repo name.
+
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
   title: 'Gabriela Rodrigues Matias',
   tagline: 'Repositório de Estudos Individuais',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: `https://${organizationName}.github.io`,
+  baseUrl: `/${projectName}/`,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
