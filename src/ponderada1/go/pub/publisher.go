557d7e9f5132249082935d73cd53195e564e99ff
gabrdgs <99189378+gabInteli@users.noreply.github.com>
2024-02-19 00:19:56 -0300

    add random numbers on pub

--- src/ponderada1/go/pub/publisher.go
+++ src/ponderada1/go/pub/publisher.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"time"
-
+	"math/rand"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -19,11 +19,11 @@ func main() {
 
 	for {
 		data := map[string]interface{}{
-			"CO2": 6,
-			"CO": 60,
-			"NO2": 250,
-			"MP10": 95,
-			"MP2,5": 80,
+			"CO2":  rand.Intn(100),
+			"CO":   rand.Intn(1000),
+			"NO2":  rand.Intn(500),
+			"MP10": rand.Intn(200),
+			"MP2,5": rand.Intn(200),
 		}
 
 		jsonData, err := json.Marshal(data)
